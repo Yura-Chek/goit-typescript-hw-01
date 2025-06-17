0b4c859f25f191608538ac4396c6d88a471f25ff
Chekan <ky.chekan@gmail.com>
2025-06-17 12:22:24 +0300

    task - basic

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+let Var: string | number;
+let chekbox: 'enable' | 'disable';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './style.css'
+import "./style.css";
 
 function add(num1: number, num2: number) {
-    return num1 + num2;
-  }
-  
-  console.log(add(1, 2));
-  
+  return num1 + num2;
+}
+
+console.log(add(1, 2));
